5746af9759f023bc42ab7cd9ce0d09267f9ca84d
Luis Felipe Zaguini <luisfelipezaguini@gmail.com>
2021-11-25 11:48:50 -0300

    Do not ship ignored files to EB

--- scripts/lib/zipDirectory.js
+++ scripts/lib/zipDirectory.js
@@ -1,5 +1,16 @@
 const archiver = require("archiver");
 const buffer = require("stream-buffers");
+const fs = require("fs");
+const path = require("path");
+
+const getGitIgnoreEntries = ({ source }) => {
+  const filePath = path.resolve(process.cwd(), ".gitignore");
+  const gitIgnore = fs.readFileSync(filePath).toString();
+
+  return gitIgnore
+    .split(/\r?\n/)
+    .filter((entry) => entry.startsWith(path.basename(source)));
+};
 
 const zipDirectory = ({ source }) => {
   const archive = archiver("zip", { zlib: { level: 9 } });
@@ -8,6 +19,7 @@ const zipDirectory = ({ source }) => {
   return new Promise((resolve, reject) => {
     archive.glob("**/*", {
       cwd: source,
+      ignore: getGitIgnoreEntries({ source }),
     });
 
     archive.on("error", (err) => reject(err)).pipe(stream);
