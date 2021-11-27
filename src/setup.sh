41fc439f684d0cab94cde3079784cc28503330f3
Luis Felipe Zaguini <luisfelipezaguini@gmail.com>
2021-11-27 16:09:43 -0300

    Use nginx instead of Apache

--- src/setup.sh
+++ src/setup.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-source /etc/apache2/envvars
-a2enmod rewrite
-service apache2 start
+service nginx start
 wp core install --url=$WORDPRESS_URL --title=$WORDPRESS_TITLE --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASSWORD --admin_email=$WORDPRESS_ADMIN_EMAIL --allow-root
-service apache2 stop
-exec apache2 -D FOREGROUND
+service nginx stop
+/start.sh
